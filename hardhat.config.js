761ccbfd813d5933c13992e5eeda527f5d78e301
Jacoseb <jacobsebio1@gmail.com>
2025-12-12 02:29:07 -0600

    Tenderly deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+const { vars } = require("hardhat/config");
+
+const TENDERLY_API_KEY = vars.get("TENDERLY_API_KEY");
+const DEPLOYER_PRIVATE_KEY = vars.get("DEPLOYER_PRIVATE_KEY");
+const COLLECTOR_PRIVATE_KEY = vars.get("COLLECTOR_PRIVATE_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks:{
+    tenderly:{
+      url: `https://virtual.mainnet.eu.rpc.tenderly.co/${TENDERLY_API_KEY}`,
+      accounts: [
+        DEPLOYER_PRIVATE_KEY,
+        COLLECTOR_PRIVATE_KEY
+
+      ]
+    }
+  }
 };
